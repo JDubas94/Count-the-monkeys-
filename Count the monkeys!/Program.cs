8454422fbf32cb735f1c0bd6b53e5c8905fb3d90
Julia Dubas <uliadubas94@gmail.com>
2022-07-31 22:20:07 +0300

    Add project files.

---
+++ Count the monkeys!/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_the_monkeys_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of monkeys!");
+
+           int number = int.Parse(Console.ReadLine());
+
+            int[] newArray = new int[number];
+
+            for (int i = 0; i < newArray.Length; i++) 
+            {
+                newArray[i] = i +1;
+                
+                Console.WriteLine(newArray[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
